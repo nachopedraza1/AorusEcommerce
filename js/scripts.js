5f96f17e0ed173c71e5ac82a4b24a520e2baf649
naachopedraza1 <nachi_choco@live.com.ar>
2022-07-04 20:07:09 -0300

    cambios 4/7

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+//menu 
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("is-active")
+});
+
+const btnMenu = document.getElementById("btnMenu");
+const menu = document.getElementById("menu");
+
+btnMenu.addEventListener("click", function () {
+    menu.classList.toggle("menu-active");
+});
+
+
+class Producto {
+    constructor(nombre, precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
+
+let productos = [
+    { nombre: "Motherboard Gigabyte B660M DS3H DDR4 S1700", precio: 28000, img: "img/productos/mother-1.png" },
+    { nombre: "Motherboard Gigabyte B660M DS3H DDR4 S1700", precio: 28000, img: "img/productos/mother-2.png" },
+    { nombre: "Motherboard Gigabyte B660M DS3H DDR4 S1700", precio: 28000, img: "img/productos/mother-3.png" },
+    { nombre: "Motherboard Gigabyte B660M DS3H DDR4 S1700", precio: 28000, img: "img/productos/mother-1.png" },
+    { nombre: "Motherboard Gigabyte B660M DS3H DDR4 S1700", precio: 28000, img: "img/productos/mother-2.png" }
+];
+
+for (const producto of productos) {
+    let productoCard = document.querySelector("#productosBox");
+    let card = document.createElement("div");
+    card.className = "border rounded-1 box-shw row col-6 col-sm-3 p-0";
+    card.innerHTML = `<div><img src="${producto.img}" class="card-img-bottom bg-black" alt="..."></div>
+                    <div class="card-body p-2">
+                        <h5 class="card-title">${"$"+producto.precio}</h5>
+                        <p class="card-text">${producto.nombre}</p>
+                    </div>`;
+
+    productoCard.append(card);
+}
+
+
+
+
+
+
+
+
